e2b30146a7dd7777c02cf5fdab4f89a70a916fb2
TEAM TSHAKE <nadoo.ali.15.4@gmail.com>
2018-01-10 00:51:18 +0100

    Update TSHAKE.sh

--- TSHAKE.sh
+++ TSHAKE.sh
@@ -1,9 +1,6 @@
-#!/bin/bash 
- COUNTER=0
-while [  $COUNTER -lt 5 ]; do
-kill $(pgrep telegram-cli)
+#!/usr/bin/env bash
 function print_logo() {
-	echo -e "\e[38;5;77m"   
+  echo -e "\e[38;5;77m"   
 echo -e "       CH > @TshAkETEAM            "
 echo -e "       CH > @TshAkETEAM           "
 echo -e "       CH > @TshAkETEAM    "
@@ -12,27 +9,28 @@ echo -e "       CH > @TshAkETEAM          \e[38;5;88m"
 echo -e ""
 echo -e ""
 echo -e ""
-echo -e "\e[33m        _____    _        _    _    _____    Dev @lIMyIl\e[0m"
-echo -e "\e[33m       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL\e[0m"
-echo -e "\e[33m         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI\e[0m"
-echo -e "\e[33m         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173\e[0m"
-echo -e "\e[33m         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl\e[0m"
+echo -e "\e[33m        _    _        _    _    _    Dev @lIMyIl\e[0m"
+echo -e "\e[33m       |_   _|| |    / \  | | _| __|   Dev @EMADOFFICAL\e[0m"
+echo -e "\e[33m         | |/ | '_ \  / _ \ | |/ /  _|     Dev @IX00XI\e[0m"
+echo -e "\e[33m         | |\__ \ | | |/ _ \|   <| |_    Dev @H_173\e[0m"
+echo -e "\e[33m         |_||_/_| |_/_/   \_\_|\_\___|   Dev @lIESIl\e[0m"
 echo -e "\e[33m                                             Dev @h_k_a\e[0m"
 }
-sleep 2
+
 if [ ! -f ./tg ]; then
 echo -e ""
-echo -e "\e[33m        _____    _        _    _    _____    Dev @lIMyIl"
-echo -e "\e[33m       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL"
-echo -e "\e[33m         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI"
-echo -e "\e[33m         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173"
-echo -e "\e[33m         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl"
+echo -e "\e[33m        _    _        _    _    _    Dev @lIMyIl"
+echo -e "\e[33m       |_   _|| |    / \  | | _| __|   Dev @EMADOFFICAL"
+echo -e "\e[33m         | |/ | '_ \  / _ \ | |/ /  _|     Dev @IX00XI"
+echo -e "\e[33m         | |\__ \ | | |/ _ \|   <| |_    Dev @H_173"
+echo -e "\e[33m         |_||_/_| |_/_/   \_\_|\_\___|   Dev @lIESIl"
 echo -e "\e[33m                                             Dev @h_k_a"
-    echo "tg لا يوجد ملف"
+    echo "\e[31;1mtg not found"
     echo "Run $0 install"
     exit 1
  fi
- sleep 2
+
+
   print_logo
    echo -e ""
 echo -e ""
@@ -47,6 +45,4 @@ echo -e "        Dev | @EMADOFFICAL"
 echo -e "        Dev | @h_k_a"
 echo -e "        \e[38;5;40m"
 
-   ./tg -s ./TSHAKE.lua
-sleep 3
-done
+lua start.lua
