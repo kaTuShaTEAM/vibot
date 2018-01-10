19892a55bdacbc1e8001ebbd2e9ba57c6b9befa4
TEAM TSHAKE <nadoo.ali.15.4@gmail.com>
2018-01-10 01:41:50 +0100

    Update TSHAKE.sh

--- TSHAKE.sh
+++ TSHAKE.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash 
+ COUNTER=0
+while [  $COUNTER -lt 5 ]; do
+kill $(pgrep telegram-cli)
 function print_logo() {
-  echo -e "\e[38;5;77m"   
+	echo -e "\e[38;5;77m"   
 echo -e "       CH > @TshAkETEAM            "
 echo -e "       CH > @TshAkETEAM           "
 echo -e "       CH > @TshAkETEAM    "
@@ -16,21 +19,20 @@ echo -e "\e[33m         | |\__ \ | | |/ _ \|   <| |_    \e[0m"
 echo -e "\e[33m         |_||_/_| |_/_/   \_\_|\_\___|   \e[0m"
 echo -e "\e[33m                                             \e[0m"
 }
-
+sleep 2
 if [ ! -f ./tg ]; then
 echo -e ""
-echo -e "\e[33m        _    _        _    _    _    "
-echo -e "\e[33m       |_   _|| |    / \  | | _| __|   "
-echo -e "\e[33m         | |/ | '_ \  / _ \ | |/ /  _|     "
-echo -e "\e[33m         | |\__ \ | | |/ _ \|   <| |_    "
-echo -e "\e[33m         |_||_/_| |_/_/   \_\_|\_\___| "
-echo -e "\e[33m                                             "
-    echo "\e[31;1mtg not found"
+echo -e "\e[33m        _    _        _    _    _    \e[0m"
+echo -e "\e[33m       |_   _|| |    / \  | | _| __|   \e[0m"
+echo -e "\e[33m         | |/ | '_ \  / _ \ | |/ /  _|     \e[0m"
+echo -e "\e[33m         | |\__ \ | | |/ _ \|   <| |_    \e[0m"
+echo -e "\e[33m         |_||_/_| |_/_/   \_\_|\_\___|   \e[0m"
+echo -e "\e[33m                                             \e[0m"
+    echo "tg لا يوجد ملف"
     echo "Run $0 install"
     exit 1
  fi
-
-
+ sleep 2
   print_logo
    echo -e ""
 echo -e ""
@@ -45,4 +47,6 @@ echo -e "        CH  | @TshAkETEAM"
 echo -e "        CH  | @TshAkETEAM"
 echo -e "        \e[38;5;40m"
 
-  ./tg -s TSHAKE.lua 
+   ./tg -s ./TSHAKE.lua
+sleep 3
+done
