089b4d4af8810f0294ca508245969ebb48360b31
محمد <alm3lm.ms@gmail.com>
2018-05-24 01:59:12 +0300

    Create vibot.sh

---
+++ vibot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+ COUNTER=0
+while [  $COUNTER -lt 5 ]; do
+kill $(pgrep telegram-cli)
+function print_logo() {
+	echo -e "\e[38;5;77m"   }
+sleep 2
+if [ ! -f ./tg ]; then
+echo -e ""
+
+echo -e "        CH  | @lgost"
+echo -e "        \e[38;5;40m"
+
+   ./tg -s ./vibot.lua
+sleep 3
+done
+
